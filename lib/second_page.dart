ec431c6abe482b9e876c60474aa94ee3b57c9513
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-20 04:12:45 +0500

    donate

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatefulWidget {
@@ -10,8 +12,48 @@ class SecondPage extends StatefulWidget {
 class _SecondPageState extends State<SecondPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(),
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.only(top: 8, left: 27, right: 27),
+          child: Column(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Welcome',
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        'Ehsaan Foundations',
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w800,
+                            color: Color(0xff326060)),
+                      ),
+                      CircleAvatar(
+                        backgroundColor: Color(0xff326060),
+                        radius: 22,
+                      )
+                    ],
+                  ),
+                  Container(
+                    width: 325,
+                    height: 1,
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
