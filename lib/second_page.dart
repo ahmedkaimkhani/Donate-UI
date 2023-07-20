85a48ee64d31a5f567d3780b65fda36072538e65
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-20 05:05:58 +0500

    donate

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -12,9 +12,10 @@ class SecondPage extends StatefulWidget {
 class _SecondPageState extends State<SecondPage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Padding(
+    return Scaffold(
+        body: SafeArea(
+      child: SingleChildScrollView(
+        child: Padding(
           padding: const EdgeInsets.only(top: 8, left: 27, right: 27),
           child: Column(
             children: [
@@ -84,30 +85,52 @@ class _SecondPageState extends State<SecondPage> {
                     width: 325,
                     height: 82,
                     decoration: BoxDecoration(
-                      color: const Color(0xff326060),
-                      borderRadius: BorderRadius.circular(20)
-                    ),
+                        color: const Color(0xff326060),
+                        borderRadius: BorderRadius.circular(20)),
                   ),
                   const SizedBox(height: 20),
                   Container(
                     width: 325,
                     height: 82,
                     decoration: BoxDecoration(
-                      color: const Color(0xff326060),
-                      borderRadius: BorderRadius.circular(20)
-                    ),
+                        color: const Color(0xff326060),
+                        borderRadius: BorderRadius.circular(20)),
                   ),
                   const SizedBox(height: 20),
-                   Divider(
+                  Divider(
                     height: 1,
                     color: Colors.grey[600],
                   ),
+                  const SizedBox(height: 5),
+                  const Center(
+                    child: Text(
+                      'Request for Donation',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  const SizedBox(height: 92),
+                  Container(
+                    width: 325,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        color: const Color(0xff326060),
+                        borderRadius: BorderRadius.circular(5)),
+                    child: const Text(
+                      'Request Now',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
                 ],
               )
             ],
           ),
         ),
       ),
-    );
+    ));
   }
 }
