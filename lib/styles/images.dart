d957ba9e3abafe3b46ebaf7209f785a1f2cd2773
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-25 04:02:05 +0500

    donate

---
+++ lib/styles/images.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Images {
+  static const String image = 'images/image.png';
+  static const String image2 = 'images/image2.png';
+  static const String image3 = 'images/image3.png';
+  static const String image4 = 'images/image4.png';
+}
