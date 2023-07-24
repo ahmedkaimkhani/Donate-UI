d057a72afa1e78d49d7b617d557bbffbccdd5312
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-25 01:20:44 +0500

    donate

---
+++ lib/styles/custom_text_style.dart
@@ -0,0 +1,10 @@
+import 'package:donate_ui_flutter/styles/styling.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextStyle {
+    final font_20 = const TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w800,
+    color: Styling.primaryColor,
+  );
+}
