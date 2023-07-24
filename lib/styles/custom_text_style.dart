98d482e6fa65934ab92f4563bed7d91b9611e7f9
ahmedkaimkhani <ak9302359@gmail.com>
2023-07-25 01:44:23 +0500

    donate

--- lib/styles/custom_text_style.dart
+++ lib/styles/custom_text_style.dart
@@ -2,9 +2,25 @@ import 'package:donate_ui_flutter/styles/styling.dart';
 import 'package:flutter/material.dart';
 
 class CustomTextStyle {
-    final font_20 = const TextStyle(
+  final black = const TextStyle(
+      fontSize: 24, fontWeight: FontWeight.w700, color: Colors.black);
+
+  final font_24 = const TextStyle(
     fontSize: 24,
     fontWeight: FontWeight.w800,
     color: Styling.primaryColor,
   );
+
+  final font_10 = const TextStyle(
+      fontSize: 10, fontWeight: FontWeight.w700, color: Colors.black);
 }
+  final font_48 = TextStyle(
+    fontSize: 48,
+    fontWeight: FontWeight.w700,
+    color: Styling.textfieldsColor,
+  );
+  final font_20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    color: Styling.textfieldsColor
+  );
